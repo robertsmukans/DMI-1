75cf39b4f0da40fb333f35686dd831759939fced
rodzhers69@gmail.com <intsdronka@gmail.com>
2017-11-29 11:55:44 +0200

    2017_11_29

---
+++ python/python_diary_20171129.py
@@ -0,0 +1,151 @@
+Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> echo teksts
+SyntaxError: invalid syntax
+>>> print "teksts"
+teksts
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__name__': '__main__', '__doc__': None, '__package__': None}
+>>> vars.
+SyntaxError: invalid syntax
+>>> vars.__doc__
+'vars([object]) -> dictionary\n\nWithout arguments, equivalent to locals().\nWith an argument, equivalent to object.__dict__.'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+>>> 
+>>>  x = 5
+ 
+  File "<pyshell#51>", line 1
+    x = 5
+    ^
+IndentationError: unexpected indent
+>>> 
+>>> x=5
+>>> print x.__doc__
+int(x=0) -> int or long
+int(x, base=10) -> int or long
+
+Convert a number or string to an integer, or return 0 if no arguments
+are given.  If x is floating point, the conversion truncates towards zero.
+If x is outside the integer range, the function returns a long instead.
+
+If x is not a number or if base is given, then x must be a string or
+Unicode object representing an integer literal in the given base.  The
+literal can be preceded by '+' or '-' and be surrounded by whitespace.
+The base defaults to 10.  Valid bases are 0 and 2-36.  Base 0 means to
+interpret the base from the string as an integer literal.
+>>> int('0b100', base=0)
+4
+>>> sin(0.56)
+
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    sin(0.56)
+NameError: name 'sin' is not defined
+>>> print input.__doc__
+input([prompt]) -> value
+
+Equivalent to eval(raw_input(prompt)).
+>>> input ("ievadiet skaitli")
+ievadiet skaitli4
+4
+>>> from math import sin
+>>> vars
+<built-in function vars>
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__package__': None, 'x': 5, '__name__': '__main__', 'sin': <built-in function sin>, '__doc__': None}
+>>> jkrom math import sin sa
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> from math import sin as sinuss
+>>> sinuss(0.56)
+0.5311861979208834
+>>> isn
+
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    isn
+NameError: name 'isn' is not defined
+>>> sin(1)
+0.8414709848078965
+>>> sin(30)
+-0.9880316240928618
+>>> sin(0.5)
+0.479425538604203
+>>> import math
+>>> cos(4)
+
+Traceback (most recent call last):
+  File "<pyshell#71>", line 1, in <module>
+    cos(4)
+NameError: name 'cos' is not defined
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__package__': None, 'x': 5, '__name__': '__main__', 'sinuss': <built-in function sin>, 'sin': <built-in function sin>, '__doc__': None, 'math': <module 'math' (built-in)>}
+>>> math.cos(0.45)
+0.9004471023526769
+>>> cos(0)
+
+Traceback (most recent call last):
+  File "<pyshell#74>", line 1, in <module>
+    cos(0)
+NameError: name 'cos' is not defined
+>>> 
+>>> sin(0)
+0.0
+>>> from math import *
+>>> vasr()
+
+Traceback (most recent call last):
+  File "<pyshell#78>", line 1, in <module>
+    vasr()
+NameError: name 'vasr' is not defined
+>>> vars()
+{'exp': <built-in function exp>, 'pow': <built-in function pow>, 'fsum': <built-in function fsum>, 'cosh': <built-in function cosh>, 'ldexp': <built-in function ldexp>, 'hypot': <built-in function hypot>, 'acosh': <built-in function acosh>, 'tan': <built-in function tan>, 'asin': <built-in function asin>, 'isnan': <built-in function isnan>, 'log': <built-in function log>, 'fabs': <built-in function fabs>, 'floor': <built-in function floor>, 'atanh': <built-in function atanh>, 'sqrt': <built-in function sqrt>, '__package__': None, 'frexp': <built-in function frexp>, 'factorial': <built-in function factorial>, 'degrees': <built-in function degrees>, 'pi': 3.141592653589793, 'log10': <built-in function log10>, 'sin': <built-in function sin>, 'math': <module 'math' (built-in)>, 'asinh': <built-in function asinh>, 'fmod': <built-in function fmod>, 'atan': <built-in function atan>, '__builtins__': <module '__builtin__' (built-in)>, 'copysign': <built-in function copysign>, 'cos': <built-in function cos>, 'ceil': <built-in function ceil>, 'atan2': <built-in function atan2>, 'isinf': <built-in function isinf>, '__doc__': None, 'sinh': <built-in function sinh>, '__name__': '__main__', 'trunc': <built-in function trunc>, 'expm1': <built-in function expm1>, 'e': 2.718281828459045, 'tanh': <built-in function tanh>, 'radians': <built-in function radians>, 'sinuss': <built-in function sin>, 'lgamma': <built-in function lgamma>, 'erf': <built-in function erf>, 'erfc': <built-in function erfc>, 'modf': <built-in function modf>, 'x': 5, 'acos': <built-in function acos>, 'log1p': <built-in function log1p>, 'gamma': <built-in function gamma>}
+>>> import math as abc
+>>> 
