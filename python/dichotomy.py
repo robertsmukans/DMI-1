7c854cc9c7d8d660139a480681378a518ca02e7f
rodzhers69@gmail.com <intsdronka@gmail.com>
2017-12-06 11:47:01 +0200

    2017_12_06

---
+++ python/dichotomy.py
@@ -0,0 +1,49 @@
+#-*- coding: utf-8 -*-
+import numpy as np #1
+import matplotlib.pyplot as plt #2
+
+
+def mans_sinuss(x): #6a
+    k = 0
+    a = (-1)**0*x**1/(1)
+    S = a
+
+    while k < 500:
+        k = k + 1
+        a = a*(-1)*x**2/((2*k)*(2*k+1))
+        S = S + a
+
+    return S
+a = 1.57 #3
+b = 4.71 #4
+#funkcijas zimesana
+x = np.arange(a,b,0.01) #5
+y = mans_sinuss(x) #6a
+plt.plot(x,y) #7
+plt.grid() #8
+plt.show() #9
+
+#funkcijas saknes meklēšana
+delta_x = 0.001
+funa = mans_sinuss(a)
+funb = mans_sinuss(b)
+if funa * funb > 0:
+    print "Intervālā [%.2f,%.2f] funkcijai nav saknes"%(a,b)
+    print "vai funkcijasi šajā intervālā ir pāru sakņu"
+    exit
+
+print "uz robežām f(%.2f)=%.2f f(%.2f)=%.2f"%(a,funa,b,funb)
+k = 0
+while b-a > delta_x:
+    k = k + 1
+    x = ( a + b )/2
+    funx = mans_sinuss(x)
+    print "f(%3d.   a=%.4f f(%.4f)=%7.4f b=%.4f"%(k,a,x,funx,b)
+    if funa * funx > 0:
+        a = x
+    else:
+        b = x 
+print "a=%.4f f(%.4f)=%7.4f b=%.4f"%(a,x,funx,b)
+print "iteraciju skaits: %d"%(k) 
+
+    
